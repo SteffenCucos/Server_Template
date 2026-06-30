4d3a4f5a9441116ebd475cfcc2d8810ff63ce72a
SteffenCucos <40307614+SteffenCucos@users.noreply.github.com>
2026-06-30 17:49:45 -0400

    Export repository dependency helpers

--- server/db/__init__.py
+++ server/db/__init__.py
@@ -7,6 +7,15 @@ repository implementations own Mongo/Postgres connection details.
 
 from .config import DatabaseBackend, DatabaseSettings
 from .database import Database, DatabaseUpdate, IdValue
+from .dependencies import (
+    PSerializeEntitySerializer,
+    SessionRepository,
+    UserRepository,
+    get_database_settings,
+    get_session_repository,
+    get_user_repository,
+    repository_dependency,
+)
 from .factory import create_repository
 from .repository import EntitySerializer, MappingSerializer, Repository
 
@@ -18,6 +27,13 @@ __all__ = [
     "EntitySerializer",
     "IdValue",
     "MappingSerializer",
+    "PSerializeEntitySerializer",
     "Repository",
+    "SessionRepository",
+    "UserRepository",
     "create_repository",
+    "get_database_settings",
+    "get_session_repository",
+    "get_user_repository",
+    "repository_dependency",
 ]
