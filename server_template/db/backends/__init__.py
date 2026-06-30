47c76dedc7f15251450e85c576d0e304ccb52b78
SteffenCucos <40307614+SteffenCucos@users.noreply.github.com>
2026-06-30 17:04:59 -0400

    Remove misplaced db backend package

--- server_template/db/backends/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Concrete database backend implementations.
-
-Do not import from this package in endpoint code unless you explicitly need to
-wire infrastructure. Application code should prefer the backend-neutral
-Repository protocol from server_template.db.repository.
-"""
