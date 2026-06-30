6e1fcb3564c3d127b7a151d9cce9b9b14f876ae9
SteffenCucos <40307614+SteffenCucos@users.noreply.github.com>
2026-06-30 17:03:29 -0400

    Move database backend package under server

---
+++ server/db/backends/__init__.py
@@ -0,0 +1,6 @@
+"""Concrete database backend implementations.
+
+Do not import from this package in endpoint code unless you explicitly need to
+wire infrastructure. Application code should prefer the backend-neutral
+Repository protocol from server.db.repository.
+"""
