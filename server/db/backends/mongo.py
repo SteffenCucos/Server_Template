04e268b17fb2193d1243bf04101302dec8a00ea0
SteffenCucos <40307614+SteffenCucos@users.noreply.github.com>
2026-07-01 01:03:44 -0400

    Use connection package for Mongo client creation

--- server/db/backends/mongo.py
+++ server/db/backends/mongo.py
@@ -5,10 +5,8 @@ from __future__ import annotations
 from collections.abc import Mapping
 from typing import Any, Generic
 
+from ..connection import create_mongo_client
 from ..repository import EntityIdRequiredError, EntitySerializer, EntityT
-
-MEMORY_MONGO_URIS = {"memory://", "mongo://memory", "mongodb://memory", "mongodb://in-memory"}
-_SHARED_MEMORY_CLIENTS: dict[str, object] = {}
 
 
 class MongoRepository(Generic[EntityT]):
@@ -27,7 +25,7 @@ class MongoRepository(Generic[EntityT]):
         serializer: EntitySerializer[EntityT],
         id_field: str = "id",
     ) -> None:
-        self._client, self._owns_client = _create_mongo_client(uri)
+        self._client, self._owns_client = create_mongo_client(uri)
         self._collection = self._client[database][collection]
         self._serializer = serializer
         self._id_field = id_field
@@ -110,16 +108,3 @@ class MongoRepository(Generic[EntityT]):
         if "_id" in public_record:
             public_record[self._id_field] = str(public_record.pop("_id"))
         return self._serializer.from_record(public_record)
-
-
-def _create_mongo_client(uri: str):
-    if uri in MEMORY_MONGO_URIS:
-        from pymongo_inmemory import MongoClient
-
-        if uri not in _SHARED_MEMORY_CLIENTS:
-            _SHARED_MEMORY_CLIENTS[uri] = MongoClient()
-        return _SHARED_MEMORY_CLIENTS[uri], False
-
-    from pymongo import MongoClient
-
-    return MongoClient(uri), True
