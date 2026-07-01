8d77c384dac90aa7dc6a9fc81423b226f6ebe1c6
SteffenCucos <40307614+SteffenCucos@users.noreply.github.com>
2026-06-30 22:43:06 -0400

    Inject DAOs into services

--- server/service/dependencies.py
+++ server/service/dependencies.py
@@ -1,14 +1,14 @@
 """FastAPI dependency providers for application services."""
 
-from db.dependencies import SessionRepository, UserRepository
+from db.dependencies import SessionDAODep, UserDAODep
 
 from .session_service import SessionService
 from .user_service import UserService
 
 
-def get_user_service(user_repository: UserRepository) -> UserService:
-    return UserService(user_repository)
+def get_user_service(user_dao: UserDAODep) -> UserService:
+    return UserService(user_dao)
 
 
-def get_session_service(session_repository: SessionRepository) -> SessionService:
-    return SessionService(session_repository)
+def get_session_service(session_dao: SessionDAODep) -> SessionService:
+    return SessionService(session_dao)
