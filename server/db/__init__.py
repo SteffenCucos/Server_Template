763ed7e6d164010a429f042373a9ff8aa8bb202a
SteffenCucos <40307614+SteffenCucos@users.noreply.github.com>
2026-07-04 18:37:23 -0400

    Export RBAC helpers

--- server/db/__init__.py
+++ server/db/__init__.py
@@ -1,40 +1,22 @@
-"""Database abstraction layer.
-
-Application code should depend on entity DAOs and repository interfaces while
-concrete repository implementations own Mongo/Postgres/SQLite connection details.
-"""
+"""Database abstraction layer."""
 
 from .config import DatabaseBackend, DatabaseSettings
 from .dependencies import (
     get_database_settings,
+    get_perm_dao,
+    get_role_dao,
+    get_role_perm_dao,
     get_session_dao,
     get_session_repository,
     get_user_dao,
     get_user_repository,
+    get_user_role_dao,
     repository_dependency,
 )
 from .entity_dao import EntityDAO
 from .factory import create_repository
 from .pserialize_entity_serializer import PSerializeEntitySerializer
+from .rbac_dao import PermDAO, RoleDAO, RolePermDAO, UserRoleDAO
 from .repository import EntitySerializer, MappingSerializer, Repository
 from .session_dao import SessionDAO
 from .user_dao import UserDAO
-
-__all__ = [
-    "DatabaseBackend",
-    "DatabaseSettings",
-    "EntityDAO",
-    "EntitySerializer",
-    "MappingSerializer",
-    "PSerializeEntitySerializer",
-    "Repository",
-    "SessionDAO",
-    "UserDAO",
-    "create_repository",
-    "get_database_settings",
-    "get_session_dao",
-    "get_session_repository",
-    "get_user_dao",
-    "get_user_repository",
-    "repository_dependency",
-]
