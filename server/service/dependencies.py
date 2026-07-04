8050b66d1362e92ae4d5b028b2f1370584e712e2
SteffenCucos <40307614+SteffenCucos@users.noreply.github.com>
2026-07-04 18:36:35 -0400

    Wire authz service

--- server/service/dependencies.py
+++ server/service/dependencies.py
@@ -2,22 +2,34 @@
 
 from typing import Annotated
 
-from db.dependencies import get_session_dao, get_user_dao
+from db.dependencies import (
+    get_perm_dao,
+    get_role_perm_dao,
+    get_session_dao,
+    get_user_dao,
+    get_user_role_dao,
+)
+from db.rbac_dao import PermDAO, RolePermDAO, UserRoleDAO
 from db.session_dao import SessionDAO
 from db.user_dao import UserDAO
 from fastapi import Depends
 
+from .authorization_service import AuthorizationService
 from .session_service import SessionService
 from .user_service import UserService
 
 
-def get_user_service(
-    user_dao: Annotated[UserDAO, Depends(get_user_dao)],
-) -> UserService:
+def get_user_service(user_dao: Annotated[UserDAO, Depends(get_user_dao)]) -> UserService:
     return UserService(user_dao)
 
 
-def get_session_service(
-    session_dao: Annotated[SessionDAO, Depends(get_session_dao)],
-) -> SessionService:
+def get_session_service(session_dao: Annotated[SessionDAO, Depends(get_session_dao)]) -> SessionService:
     return SessionService(session_dao)
+
+
+def get_authz_service(
+    user_role_dao: Annotated[UserRoleDAO, Depends(get_user_role_dao)],
+    role_perm_dao: Annotated[RolePermDAO, Depends(get_role_perm_dao)],
+    perm_dao: Annotated[PermDAO, Depends(get_perm_dao)],
+) -> AuthorizationService:
+    return AuthorizationService(user_role_dao, role_perm_dao, perm_dao)
