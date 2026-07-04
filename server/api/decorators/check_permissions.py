72e8b3ef041bebfd65cef91f489a4e4356874614
SteffenCucos <40307614+SteffenCucos@users.noreply.github.com>
2026-07-04 18:37:03 -0400

    Use RBAC access checks

--- server/api/decorators/check_permissions.py
+++ server/api/decorators/check_permissions.py
@@ -2,35 +2,63 @@ import logging
 
 from api.decorators.decorator import decorator
 from api.exceptions import ForbiddenException
+from auth.rbac import Permission as PermModel
+from auth.rbac import RolePermission, UserRole
+from db import DatabaseSettings, PSerializeEntitySerializer, create_repository
+from db.rbac_dao import PermDAO, RolePermDAO, UserRoleDAO
 from models.request_context import RequestContext
+from service.authorization_service import AuthorizationService
 from starlette.requests import Request
 
 logger = logging.getLogger()
 
-def check_permission(permission: str=None):
-    '''Ensures that the calling user has sufficient permissions to access the resource
 
-    Args:
-        permission (str): A format string representing the required permission for accessing the resource.
-            It can reference named parameters of the request. Ie:
-                @post('/something/{someId}')
-                @check_permission('delete/{someId}')
-    
-    Raises:
-        api.exceptions.ForbiddenException: If the calling user does not have sufficient permissions
-
-    '''
+def check_permission(permission: str = None):
     def check_permission_wrapper(request: Request, *positional, **named):
         request_context = RequestContext.get_context()
-        user =  request_context.current_user
+        user = request_context.current_user
 
         if not user:
-            logger.info("Permission check failed: No user in request context")
-            raise ForbiddenException("You don't have permission to access this resource")
+            logger.info("Access check failed: No user in request context")
+            raise ForbiddenException("You don't have access to this resource")
 
         filled_in = permission.format(**named)
-        if permission and not user.has_permission(filled_in):
-            logger.info("Permission check failed: No matching permission string")
-            raise ForbiddenException("You don't have permission to access this resource")
+        if permission and not _user_has_access(user._id, filled_in):
+            logger.info("Access check failed: No matching RBAC grant")
+            raise ForbiddenException("You don't have access to this resource")
 
     return decorator(pre=check_permission_wrapper)
+
+
+def _user_has_access(user_id: object, required: str) -> bool:
+    settings = DatabaseSettings.from_env()
+    user_role_repository = create_repository(
+        settings=settings,
+        resource_name="user_roles",
+        serializer=PSerializeEntitySerializer(UserRole),
+        id_field="_id",
+    )
+    role_perm_repository = create_repository(
+        settings=settings,
+        resource_name="role_perms",
+        serializer=PSerializeEntitySerializer(RolePermission),
+        id_field="_id",
+    )
+    perm_repository = create_repository(
+        settings=settings,
+        resource_name="perms",
+        serializer=PSerializeEntitySerializer(PermModel),
+        id_field="_id",
+    )
+
+    try:
+        service = AuthorizationService(
+            UserRoleDAO(user_role_repository),
+            RolePermDAO(role_perm_repository),
+            PermDAO(perm_repository),
+        )
+        return service.user_has_access(user_id, required)
+    finally:
+        user_role_repository.close()
+        role_perm_repository.close()
+        perm_repository.close()
