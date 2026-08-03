0a0282ccdc9e9878e12b56339d0476671b450495
SteffenCucos <40307614+SteffenCucos@users.noreply.github.com>
2026-08-02 22:21:32 -0400

    Make authenticated annotation metadata-only

--- server/api/decorators/authenticated.py
+++ server/api/decorators/authenticated.py
@@ -1,31 +1,8 @@
-import logging
+"""Backward-compatible authentication annotation."""
 
-from api.decorators.decorator import decorator
-from api.exceptions import UnauthorizedException
-from models.request_context import RequestContext
-from starlette.requests import Request
+from api.auth import requires_auth
 
-logger = logging.getLogger()
 
 def authenticated():
-    '''Ensures the caller has a valid session.
-
-    Raises: 
-        api.exceptions.UnauthorizedException: If the caller can't be authenticated.
-    '''
-    def authenticate_user(request: Request, *positional, **named):
-        request_context = RequestContext.get_context()
-
-        if not request_context.current_user:
-            logger.info("Permission check failed: User does not exist")
-            raise UnauthorizedException("You don't have permission to access this resource")
-
-        if not request_context.session:
-            logger.info("Permission check failed: Session DNE")
-            raise UnauthorizedException("You don't have permission to access this resource")
-        
-        if request_context.session_expired:
-            logger.info("Permission check failed: Session expired")
-            raise UnauthorizedException("You don't have permission to access this resource")
-        
-    return decorator(pre=authenticate_user)
+    """Mark a route as authenticated using the FastAPI DI auth flow."""
+    return requires_auth
