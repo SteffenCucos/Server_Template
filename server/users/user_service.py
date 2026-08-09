d296120e8640938de55d1d7bcf7afd8c4d79c792
SteffenCucos <40307614+SteffenCucos@users.noreply.github.com>
2026-08-09 14:08:15 -0400

    Add user update service

--- server/users/user_service.py
+++ server/users/user_service.py
@@ -16,6 +16,13 @@ class CreateUserRequest:
     email: str
 
 
+@dataclass
+class UpdateUserRequest:
+    user_name: str | None = None
+    password: str | None = None
+    email: str | None = None
+
+
 class UserService:
     def __init__(self, user_dao: UserDAO):
         self.user_dao = user_dao
@@ -42,6 +49,30 @@ class UserService:
 
     async def get_all_users(self) -> list[User]:
         return await self.user_dao.list()
+
+    async def update_user(self, user: User, user_request: UpdateUserRequest) -> User:
+        changes: dict[str, str] = {}
+
+        if user_request.user_name is not None and user_request.user_name != user.user_name:
+            existing_user = await self.user_dao.get_by_name(user_request.user_name)
+            if existing_user and existing_user._id != user._id:
+                raise UnprocessableEntityException("Username is already taken")
+            changes["user_name"] = user_request.user_name
+
+        if user_request.email is not None and user_request.email != user.email:
+            existing_user = await self.user_dao.get_by_email(user_request.email)
+            if existing_user and existing_user._id != user._id:
+                raise UnprocessableEntityException("Email is already in use.")
+            changes["email"] = user_request.email
+
+        if user_request.password is not None and user_request.password != user.password:
+            changes["password"] = user_request.password
+
+        if not changes:
+            return user
+
+        updated_user = await self.user_dao.update(user._id, changes)
+        return updated_user or user
 
     async def delete_user(self, user: User) -> bool:
         return await self.user_dao.delete(user._id)
